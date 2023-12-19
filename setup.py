4c961e622daa260b267acac8cc37f58e2248ddd2
miskamero <miskamero@users.noreply.github.com>
2023-12-19 12:17:49 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='klinoff-math',
+    version='0.1.0',
+    description='A simple klinoff math library',
+    author='Klinoff TMS',
+    py_modules=['klinoffmath'],
+)
