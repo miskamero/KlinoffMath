ec431bf6ee42d88e840b80b8ab6cfa911d60f1e1
miskamero <miska.mero@gmail.com>
2024-01-11 08:56:18 +0200

    klinoff math 0.1.2

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@ from setuptools import setup
 
 setup(
     name='klinoff-math',
-    version='0.1.1',
-    description='A simple klinoff math library',
-    author='Klinoff TMS',
+    version='0.1.2',
+    description='A simple klinoff math library by Klinoff Corp (See Github)',
+    author='Klinoff Corp',
     py_modules=['klinoffmath'],
     install_requires=['simpleaudio'],
+    package_data={'': ['*.wav']},
+    include_package_data=True,
 )
